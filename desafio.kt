7459972398e816dc4a1e29cf81a9ec916d49be30
Renato Alexandre <renatoacf@gmail.com>
2022-12-26 20:33:25 -0300

    Implementação e teste do desafio.

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,64 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+enum class Area { Kotlin, Java, Javascript, Flutter }
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+class Usuario(var nome: String, var email: String, var matricula: String)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, var area: Area, var nivel: Nivel)
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val usuarioJoao = Usuario("João", "joao@email.com", "abc123")
+    val usuarioMaria = Usuario("Maria", "maria@email.com", "abc234")
+    val usuarioAna = Usuario("Ana", "ana@email.com", "abc345")
+
+    val introducaoAoKotlin = ConteudoEducacional("Introdução ao Kotlin", 60, Area.Kotlin, Nivel.BASICO)
+    val ooEmKotlin = ConteudoEducacional("Orientação a Objetos em Kotlin", 180, Area.Kotlin, Nivel.BASICO)
+    val operadoresKotlin = ConteudoEducacional("Operadores em Kotlin", 50, Area.Kotlin, Nivel.BASICO)
+
+    val introducaoAoJava = ConteudoEducacional("Introdução ao Java", 40, Area.Java, Nivel.BASICO)
+    val ooEmJava = ConteudoEducacional("Orientação a Objetos em Java", 180, Area.Java, Nivel.BASICO)
+    val operadoresJava = ConteudoEducacional("Operadores em Java", 50, Area.Java, Nivel.BASICO)
+    val springBoot = ConteudoEducacional("Spring Boot", 100, Area.Java, Nivel.INTERMEDIARIO)
+
+    fun getConteudosKotlin(): List<ConteudoEducacional> {
+        return listOf(
+            introducaoAoKotlin,
+            ooEmKotlin,
+            operadoresKotlin
+        )
+    }
+
+    fun getConteudosJava(): List<ConteudoEducacional> {
+        return listOf(
+            introducaoAoJava,
+            ooEmJava,
+            operadoresJava,
+            springBoot
+        )
+    }
+
+    var formacaoKotlin = Formacao("Formação em Kotlin", getConteudosKotlin(), Nivel.BASICO)
+    var formacaoJava = Formacao("Formação em Java", getConteudosJava(), Nivel.INTERMEDIARIO)
+
+    formacaoKotlin.matricular(usuarioJoao)
+    formacaoKotlin.matricular(usuarioMaria)
+    formacaoKotlin.matricular(usuarioAna)
+
+    formacaoJava.matricular(usuarioAna)
+
+
+
 }
+
+
